17e4d0ee97b1754040da152382b843ce7e107519
Kelo <meetkelo@outlook.com>
2022-11-13 15:48:17 +0800

    disable mouse wheel left&right

--- lua/user/plugins/toggleterm.lua
+++ lua/user/plugins/toggleterm.lua
@@ -8,21 +8,17 @@ function M.setup()
     shade_terminals = false,
     insert_mappings = true,
   }
-  -- set in settings.lua
-  --vim.api.nvim_create_augroup("toggle_terminal_keymap", { clear = true })
-  --vim.api.nvim_create_autocmd("TermOpen", {
-  --  pattern = "term://*toggleterm*",
-  --  callback = function()
-  --    local utils = require("user.utils")
-  --    local opts = { buffer = 0 }
-  --    utils.keymap("t", "<esc>", [[<C-\><C-n>]], opts)
-  --    utils.keymap("t", "jj", [[<C-\><C-n>]], opts)
-  --    utils.keymap("t", "<C-h>", [[<C-\><C-n><C-w>h]], opts)
-  --    utils.keymap("t", "<C-j>", [[<C-\><C-n><C-w>j]], opts)
-  --    utils.keymap("t", "<C-k>", [[<C-\><C-n><C-w>k]], opts)
-  --    utils.keymap("t", "<C-l>", [[<C-\><C-n><C-w>l]], opts)
-  --  end
-  --})
+  vim.api.nvim_create_augroup("toggle_terminal_keymap", { clear = true })
+  vim.api.nvim_create_autocmd("TermOpen", {
+    --pattern = "term://*toggleterm*",
+    pattern = "term://*",
+    callback = function()
+      local utils = require("user.utils")
+      local opts = { buffer = 0 }
+      utils.keymap({ "t", "n" }, "<ScrollWheelLeft>", "<nop>", opts)
+      utils.keymap({ "t", "n" }, "<ScrollWheelRight>", "<nop>", opts)
+    end
+  })
 end
 
 return M
