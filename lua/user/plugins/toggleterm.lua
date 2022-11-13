b3772716a15e2e889197dbd5d89cdc128cd45042
Kelo <meetkelo@outlook.com>
2022-11-13 15:59:16 +0800

    set terminal options

--- lua/user/plugins/toggleterm.lua
+++ lua/user/plugins/toggleterm.lua
@@ -1,14 +1,7 @@
 local M = {}
 
-function M.setup()
-  local toogleterm = require("toggleterm")
-  toogleterm.setup {
-    open_mapping = [[<c-\>]],
-    autochdir = true,
-    shade_terminals = false,
-    insert_mappings = true,
-  }
-  vim.api.nvim_create_augroup("toggle_terminal_keymap", { clear = true })
+function M.set_term_opts()
+  vim.api.nvim_create_augroup("set_term_opts", { clear = true })
   vim.api.nvim_create_autocmd("TermOpen", {
     --pattern = "term://*toggleterm*",
     pattern = "term://*",
@@ -17,8 +10,22 @@ function M.setup()
       local opts = { buffer = 0 }
       utils.keymap({ "t", "n" }, "<ScrollWheelLeft>", "<nop>", opts)
       utils.keymap({ "t", "n" }, "<ScrollWheelRight>", "<nop>", opts)
+
+      vim.api.nvim_win_set_option(0, "number", false)
+      vim.api.nvim_win_set_option(0, "relativenumber", false)
     end
   })
+end
+
+function M.setup()
+  local toogleterm = require("toggleterm")
+  toogleterm.setup {
+    open_mapping = [[<c-\>]],
+    autochdir = true,
+    shade_terminals = false,
+    insert_mappings = true,
+  }
+  M.set_term_opts()
 end
 
 return M
