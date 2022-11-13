ac02fbfa2c196042af7a65da9f268484d9a94d0d
Kelo <meetkelo@outlook.com>
2022-11-13 20:40:10 +0800

    better C-;

--- lua/user/plugins/cmpletion.lua
+++ lua/user/plugins/cmpletion.lua
@@ -112,8 +112,8 @@ function M.setup()
         "s",
       }),
       ["<C-;>"] = cmp.mapping(function(fallback)
-        if luasnip.expand_or_locally_jumpable() then
-          luasnip.expand_or_jump()
+        if luasnip.jumpable(1) then
+          luasnip.jump(1)
         else
           fallback()
         end
