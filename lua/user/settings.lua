31290f5797e202f00a533f0f42e5d4d75c1a12c6
Kelo <meetkelo@outlook.com>
2022-11-17 21:19:13 +0800

    update listchars

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -5,7 +5,8 @@ function M.setup()
     fileformats = "unix,mac,dos",
     list = true,
     -- listchars="tab: »,nbsp:␣,trail:·,extends:›,precedes:‹",
-    listchars = "tab:⎸→,nbsp:␣,trail:·",
+    -- listchars = "tab:⎸→,nbsp:␣,trail:·",
+    listchars = "tab:⎸―,nbsp:␣,trail:·",
     backup = false, -- creates a backup file
     -- clipboard = "unnamedplus", -- allows neovim to access the system clipboard
     cmdheight = 1, -- more space in the neovim command line for displaying messages
