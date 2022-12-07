c6ea662079d87f7b846c8e6754e16d0c244f8f36
Kelo <meetkelo@outlook.com>
2022-12-07 09:30:10 +0800

    treesitter: incremental selection

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -55,8 +55,16 @@ function M.setup()
         json = "",
       },
     },
-    playground = {
+    incremental_selection = {
       enable = true,
+      keymaps = {
+        init_selection = "gnn",
+        node_incremental = "v",
+        node_decremental = "V",
+      },
+    },
+    playground = {
+      enable = false,
       disable = {},
       updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
       persist_queries = false, -- Whether the query persists across vim sessions
