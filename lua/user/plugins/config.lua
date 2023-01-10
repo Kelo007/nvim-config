be09fd02be2e18fc4bb83a3db4e265f2f6682333
Kelo <meetkelo@outlook.com>
2023-01-10 19:38:12 +0800

    remove yank on highlight

--- lua/user/plugins/config.lua
+++ lua/user/plugins/config.lua
@@ -2,7 +2,6 @@ local function config_once()
   if not vim.g.loaded_user_config then
     require("user.settings").setup()
     require("user.ui").setup()
-    require("user.autocmds")
     vim.g.loaded_user_config = true
   end
 end
