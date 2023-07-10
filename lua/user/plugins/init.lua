b791ce26b7130e4bf29bb5110cba3f8aaa2b9c81
Kelo <meetkelo@outlook.com>
2023-07-10 17:27:52 +0800

    feat: flash.nvim

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -23,6 +23,7 @@ return {
   {
     "phaazon/hop.nvim",
     branch = "v2", -- optional but strongly recommended
+    enabled = false,
     config = function()
       local hop = require("hop")
       hop.setup { keys = "etovxqpdygfblzhckisuran" }
